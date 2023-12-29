14b3d7c526005ee5ed93e8b5ed5de6293ca0c05b
SOUFIANE IDRISSI <idrissisoufiane2000@gmail.com>
2023-12-29 12:48:53 +0100

    handle with retouch && issues

--- src/Components/C-Purchases/Purchases.jsx
+++ src/Components/C-Purchases/Purchases.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
 import "./purchases.css";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Purchases = ({ departmentPurchase }) => {
   console.log(departmentPurchase);
+
+  const [total, setTotal] = useState(null);
+
+  const [close, setClose] = useState(false);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     if (departmentPurchase) {
@@ -21,6 +26,32 @@ const Purchases = ({ departmentPurchase }) => {
     : null;
 
   console.log(Purchaseepartments);
+
+  useEffect(() => {
+    let prices = Purchaseepartments.map((price) => parseFloat(price.pric));
+
+    let result = prices.reduce(function (acc, current) {
+      return acc + current;
+    });
+    console.log(result);
+    setTotal(result);
+  }, [Purchaseepartments]);
+
+  const closePoUp = () => {
+    if (close == false) {
+      setClose(true);
+    } else {
+      setClose(false);
+    }
+  };
+
+  const showPoUp = () => {
+    if (show == false) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
 
   return (
     // <div className="box container d-flex justify-content-between align-items-center"></div>
@@ -45,13 +76,67 @@ const Purchases = ({ departmentPurchase }) => {
                 </td>
                 <td>{Purchase.subtitle}</td>
                 <td>{Purchase.title}</td>
-                <td>{Purchase.pric} $</td>
+                <td className="prix">{Purchase.pric} $</td>
               </tr>
             ))}
+          <tr>
+            <td colSpan={"3"}>Total</td>
+            <td>{total} $</td>
+          </tr>
         </tbody>
       </table>
 
-      <button>Shopping Now</button>
+      <button onClick={showPoUp}>Shopping Now</button>
+
+      <div
+        className={`department-Buy ${show ? "effect" : ""} ${
+          close == true ? "active" : ""
+        }`}
+      >
+        <div className="box">
+          <div className="image">
+            <img src={""} alt="" />
+          </div>
+          <div className="boxcontain flex-column">
+            <div className="field-name d-flex justify-content-between align-items-center gap-3 mb-3">
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="last Name" />
+            </div>
+
+            <div className="number mb-3">
+              <input type="text" placeholder="Your-Number" />
+            </div>
+
+            <div className="field-card d-flex flex-column mb-3">
+              <div className="card">
+                <input type="text" placeholder="1234 1234 1234 1234" />
+              </div>
+              <div className="card-info d-flex justify-content-between align-items-center">
+                <input type="text" placeholder="MM / YY" />
+                <input type="text" placeholder="CVC" />
+              </div>
+            </div>
+
+            <div className="Biling-address mb-3">
+              <div className="section">
+                <select>
+                  <option value="">Casa-Blanca</option>
+                  <option value="">Settat</option>
+                  <option value="">Mohamedia</option>
+                </select>
+              </div>
+              <div className="address">
+                <input type="text" placeholder="Address Line 1" />
+                <input type="text" placeholder="Address Line 2" />
+              </div>
+            </div>
+
+            <button className="pay">Pay</button>
+          </div>
+
+          <i className="x fa-regular fa-circle-xmark" onClick={closePoUp}></i>
+        </div>
+      </div>
     </div>
   );
 };
